baf3c726f43f851fdb8d6ab0a8ef89bfb31a7d38
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-18 18:38:18 +0300

    dz01
    dz01

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace vn_csharp_dz01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int eatCount;
+            byte health;
+            bool run;
+            char symbol;
+            float money;
+            double grade;
+            short range;
+            ulong distance;
+            ushort radius;
+        }
+    }
+}
